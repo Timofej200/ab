66f407f2d2fe73d5b9f3f6f2bc41f7469e5842ab
Cab-042-n9 <Cab-042-n9@03-C42-06>
2022-11-22 09:38:25 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=12
+            int y=24
+           Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
